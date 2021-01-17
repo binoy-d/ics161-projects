adeda36c8a8115f8161a79a7da923ffacaff901a
Daniel Binoy <dbinoy15@gmail.com>
2021-01-16 18:18:37 -0800

    assignment 1

---
+++ assignment-1/MoveNShoot/Assets/WatermelonControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatermelonControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.IgnoreCollision(GameObject.Find("SmallTank").GetComponent<Collider>(), GetComponent<Collider>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy"){
+            print("enemy hit");
+            
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ assignment-1/MoveNShoot/Assets/wall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.IgnoreCollision(GameObject.Find("Ground").GetComponent<Collider>(), GetComponent<Collider>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Watermelon"){
+            print("delete melon");
+            Destroy(collision.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Watermelon"){
+            print("delete melon");
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
