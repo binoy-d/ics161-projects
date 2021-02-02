de91ac15ae96f6434de310417a4ec9a2a01d0e8b
Daniel Binoy <dbinoy15@gmail.com>
2021-02-01 20:10:42 -0800

    init assignment 2

---
+++ assignment-2/Assignment1/Assignment1/Assets/Bullet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int parentPlayerNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setParentPlayer(int pNum){
+        parentPlayerNum = pNum;
+        //get all the tanks
+        GameObject[] tanks = GameObject.FindGameObjectsWithTag("Tank");
+
+        //figure out which tank was the parent
+        //and set the color to match the parent color
+        foreach(GameObject t in tanks){
+            if(t.GetComponent<Moving>().getPlayerNum() == parentPlayerNum){
+                //set color of tip
+                gameObject.GetComponent<Renderer>().material = t.GetComponent<Renderer>().material;
+                //set color of cylinder(check prefab)
+                //sorry for this disgusting line
+                gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = t.GetComponent<Renderer>().material;
+            }
+        }
+        
+        
+    }
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("Trigger Enter: "+other.name+","+gameObject.name);
+        //destroy targets
+        if(other.tag == "Target"){
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }else if(other.tag == "Tank"){
+            //destroy tanks only if their playerNum 
+            // is not the same as this bullet's parent
+            if(other.GetComponent<Moving>().getPlayerNum()!=parentPlayerNum){
+                other.GetComponent<Moving>().killPlayer();
+                Destroy(gameObject);
+            }
+        }else{
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ assignment-2/Assignment1/Assignment1/Assets/Moving.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public GameObject bullet;
+    public int playerNum;
+    private GameObject shotBullet;
+    private float speed;
+    //{up, down, left, right, shoot}
+    private KeyCode[] controls; 
+    private bool isDead;
+    // Start is called before the first frame update
+    void Start(){
+        speed  = 0.2f;
+        isDead = false;
+        if(playerNum == 1){
+            controls = new KeyCode[]{
+                KeyCode.W,
+                KeyCode.S,
+                KeyCode.A,
+                KeyCode.D,
+                KeyCode.Space
+            };
+        }else{
+            controls = new KeyCode[]{
+                KeyCode.UpArrow,
+                KeyCode.DownArrow,
+                KeyCode.LeftArrow,
+                KeyCode.RightArrow,
+                KeyCode.Return
+            };
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(isDead){
+            //move it far far away
+            transform.position = new Vector3(200, 200, 200);
+        }
+        if(Input.GetKeyDown(controls[4])){
+            Debug.Log("Player "+playerNum+" fired!");
+            shotBullet = Instantiate(bullet,transform.position,transform.rotation);
+            shotBullet.GetComponent<Bullet>().setParentPlayer(playerNum);
+            Transform tranBullet = shotBullet.GetComponent<Transform>();
+            Rigidbody rigBullet = shotBullet.GetComponent<Rigidbody>();
+            if( rigBullet){
+                rigBullet.AddForce(transform.forward*40.0f,ForceMode.VelocityChange);
+            }
+        }
+    }
+
+    
+    void FixedUpdate(){
+        if (Input.GetKey(controls[3])){//right
+            transform.Translate(new Vector3(0,0,speed));
+            transform.rotation = Quaternion.Euler(0,90,0);
+        }else if(Input.GetKey(controls[2])){//left
+            transform.Translate(new Vector3(0,0,speed));
+            transform.rotation = Quaternion.Euler(0,-90,0);
+        }else if(Input.GetKey(controls[0])){//up
+            transform.Translate(new Vector3(0,0,speed));
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }else if(Input.GetKey(controls[1])){//down
+            transform.Translate(new Vector3(0,0,speed));
+            transform.rotation = Quaternion.Euler(0,180,0);
+        }
+        
+    }
+
+    public int getPlayerNum(){
+        return playerNum;
+    }
+
+    public void killPlayer(){
+        isDead = true;
+        Invoke("revivePlayer", 3); //revive after 3 seconds
+    }
+
+    public void revivePlayer(){
+        isDead = false;
+        transform.position = new Vector3(Random.Range(-5f, 5f), 1, Random.Range(-5f, 5f));
+    }
+    
+}
